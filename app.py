fe22fa91b40b64720288bfaf46556eeb73aaeef9
DevHayer <nguyenhieu.mktunity@gmail.com>
2025-06-05 00:44:36 +0700

    delelte file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from convert_solar_to_lunar import convert_solar_to_lunar
 
+
 app = Flask(__name__, static_folder='static', template_folder='templates')
 CORS(app)
 
